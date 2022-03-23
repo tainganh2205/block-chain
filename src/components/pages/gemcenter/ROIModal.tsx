5305e4374f8ed09dc4b18eb6758f5c66fb7af1b2
Thanh Tai <thanhtai16969@gmail.com>
2022-03-23 15:07:07 +0700

    pools

--- src/components/pages/gemcenter/ROIModal.tsx
+++ src/components/pages/gemcenter/ROIModal.tsx
@@ -57,7 +57,7 @@ export default function ROIModal(props: ROIModalProps) {
       ),
       rewardTokenPer1k: (
         <Text as="span" className={color}>
-          {rewardTokenPer1k}
+          {item.timeframe === "1 year (APY)" && !rewardTokenPer1k ? filterFloat(Number(1000)) : rewardTokenPer1k}
         </Text>
       )
     };
