dc26ca01a08419e987fa81105bdc0a5dfb5e4106
Thanh Tai <thanhtai16969@gmail.com>
2022-04-10 11:05:22 +0700

    fix bug

--- src/pages/LaunchpadPools/TopBanner.tsx
+++ src/pages/LaunchpadPools/TopBanner.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
-import UnlockButton from '../../components/ButtonV2'
+import React from "react";
+import { isMobile } from "react-device-detect";
+import { useHistory } from "react-router-dom";
+import UnlockButton from "../../components/ButtonV2";
 
 const TopBanner = () => {
 
-  const history = useHistory()
-  const typePath = history.location.search.split('?')
+  const history = useHistory();
+  const typePath = history.location.search.split("?");
 
   return (
     <div className="main-banner-v3">
@@ -15,13 +16,16 @@ const TopBanner = () => {
         </div>
       </div>
       <div className="launchpad-content">
-        <div className="text-l-banner">
+        <div className="text-l-banner" style={{ width: isMobile ? "100%" : "50%" }}>
           <h3 className="title-v3">Launchpad Pools</h3>
-          <p className="desc">Artechain Farms offer multiple farming opportunities to you. Get double rewards by staking your LP tokens in return for additional TAC tokens and earning high income from swap transactions</p>
+          <p className="desc">
+            Launchpad pools offer multiple opportunities for you. Get double rewards by staking LFW tokens in return for LFW tokens and earning eligible tickets for attractive IDO allocation on our
+            Launchpad.
+          </p>
           <p className="desc">More Details...</p>
         </div>
       </div>
     </div>
-  )
-}
-export default TopBanner
+  );
+};
+export default TopBanner;
