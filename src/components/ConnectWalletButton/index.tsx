7304421067db9a0270d0804ce471fbc0da8dd7a1
Thanh Tai <thanhtai16969@gmail.com>
2022-04-14 09:43:12 +0700

    fix bug apply ido

--- src/components/ConnectWalletButton/index.tsx
+++ src/components/ConnectWalletButton/index.tsx
@@ -6,6 +6,7 @@ import useI18n from "hooks/useI18n";
 import { ButtonArt as Button } from "components/Art";
 import { useDeepEffect } from "../../hooks";
 import axios from "axios";
+import { get } from "lodash";
 
 const { REACT_APP_API_URL } = process.env;
 const UnlockButton = (props) => {
@@ -25,8 +26,9 @@ const UnlockButton = (props) => {
   const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(handleLogin, handleLogout, account as string);
   const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null;
   useDeepEffect(() => {
-    const accessToken = localStorage.getItem("lfw-signature");
-    if (account && !accessToken) {
+    let accessToken = localStorage.getItem("lfw-signature");
+    accessToken = JSON.parse(accessToken!);
+    if ((account && !accessToken) || (accessToken && get(accessToken, "wallet", "") !== account)) {
       axios.get(`${REACT_APP_API_URL}/v1/auth/nonce?walletAddress=${account}`).then(res => {
         library?.getSigner(account).signMessage(res.data.data.nonce.toString()).then(signature => {
           axios.post(`${REACT_APP_API_URL}/v1/auth`, {
