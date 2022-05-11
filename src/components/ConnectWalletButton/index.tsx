5d8e52ef83eb21382669e72ae266cb5d54f5959b
Thanh Tai <thanhtai16969@gmail.com>
2022-05-11 15:54:50 +0700

    feat: sign message

--- src/components/ConnectWalletButton/index.tsx
+++ src/components/ConnectWalletButton/index.tsx
@@ -4,13 +4,10 @@ import { ConnectorId, useWalletModal } from "@artechain/uikit";
 import { injected, walletconnect } from "connectors";
 import useI18n from "hooks/useI18n";
 import { ButtonArt as Button } from "components/Art";
-import { useDeepEffect } from "../../hooks";
-import axios from "axios";
-const { REACT_APP_API_URL } = process.env;
 
 const UnlockButton = (props) => {
   const TranslateString = useI18n();
-  const { account, activate, deactivate, library } = useWeb3React();
+  const { account, activate, deactivate } = useWeb3React();
 
   const handleLogin = (connectorId: ConnectorId) => {
     if (connectorId === "walletconnect") {
@@ -24,20 +21,7 @@ const UnlockButton = (props) => {
   };
   const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(handleLogin, handleLogout, account as string);
   const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null;
-  useDeepEffect(() => {
-    if (account) {
-      axios.get(`${REACT_APP_API_URL}/v1/auth/nonce?walletAddress=${account}`).then(res => {
-        library?.getSigner(account).signMessage(res.data.data.nonce.toString()).then(signature => {
-          axios.post(`${REACT_APP_API_URL}/v1/auth`, {
-            signature: signature.replace("0x", ""),
-            walletAddress: account
-          }).then(response => {
-            localStorage.setItem("lfw-signature", JSON.stringify({ signature: signature.replace("Ox", ""), wallet: account, access_token: response.data.data }));
-          });
-        });
-      });
-    }
-  }, [account]);
+
   return (
     <div>
       {account ? (
